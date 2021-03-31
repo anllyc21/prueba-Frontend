bc4aa60f32e3f1dbb17831b8c2cee63d02d15d29
David Supo <david.supo.contractor@bbva.com>
2021-03-30 22:35:05 -0500

    feat(_utils.js): added methods implement

--- scripts/_utils.js
+++ scripts/_utils.js
@@ -1,3 +1,11 @@
 export function searchMusic(title = '') {
   // TODO: implement searchMusic
 }
+
+export function searchAlbums(title = '') {
+  // TODO: implement search Albums
+}
+
+export function searchArtists(name = '') {
+  // TODO: implement searchArtists
+}
